d2124a1bcde4aa687c5c0664598711957cc94b32
miguelmoramax <miguelmoramaturana@gmail.com>
2024-08-13 17:08:12 -0400

    125-validPalindrome.py

--- TwoPointers/167-TwoIntegerSumII.py
+++ TwoPointers/167-TwoIntegerSumII.py
@@ -1,3 +1,4 @@
+# 167. Two Sum II - Input Array Is Sorted
 # Given an array of integers numbers that is sorted in non-decreasing order.
 
 # Return the indices (1-indexed) of two numbers, [index1, index2], such that they add up to a given target number target and index1 < index2. Note that index1 and index2 cannot be equal, therefore you may not use the same element twice.
