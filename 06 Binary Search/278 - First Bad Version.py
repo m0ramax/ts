f3d48b025a2c49ac5138faf72b8871fa1832c6c5
miguelmoramax <miguelmoramaturana@gmail.com>
2024-08-19 15:06:47 -0400

    278. First Bad Version

--- 06 Binary Search/278 - First Bad Version.py
+++ 06 Binary Search/278 - First Bad Version.py
@@ -23,3 +23,15 @@
  
 # Constraints:
 # 1 <= bad <= n <= 231 - 1
+
+def firstBadVersion(n):
+    left, right = 1,n
+
+    while left < right:
+        mid = left + (right - left) // 2
+
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
